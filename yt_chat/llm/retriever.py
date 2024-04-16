acfef9e64b40f3c33d59bf6f95ff53e20d8ccefd
A. Cordier <40168022+acordier16@users.noreply.github.com>
2024-04-16 12:07:12 +0200

    Update yt_chat/llm/retriever.py

--- yt_chat/llm/retriever.py
+++ yt_chat/llm/retriever.py
@@ -14,5 +14,4 @@ def retriever(docs, question, n: int = 4):
     return [x for x, _ in sorted(zip(docs, similarities))][::-1][:n]
 
 def get_embedding(text, model="text-embedding-3-small"):
-   text = text.replace("\n", " ")
    return client.embeddings.create(input=[text], model=model).data[0].embedding
